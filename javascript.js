9a6fbc16e6c1a3e22800fa6ecc5c0e994f77d006
Austin T <austin.taranto@gmail.com>
2020-02-23 14:59:18 -0500

    strip function and look into regular expression

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim(); //regular expression/carrot. look into this. i = incensitive
+}
+ 
+
 const sortedBands = bands.sort(function (a, b) {
     if (a > b) {
         return 1;
